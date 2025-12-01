549bfb13c323affaaac6cdedabd01f6f6441a579
Amit Singh <amitsingh.synsoft@gmail.com>
2025-12-01 12:49:16 +0530

    homepage changes

--- src/_registry/manar/HomePage.registry.tsx
+++ src/_registry/manar/HomePage.registry.tsx
@@ -3,15 +3,19 @@
 import dynamic from 'next/dynamic'
 
 import HeroSection from '@/_components/manar/pages-components/home/hero-section/HeroSection'
-import NewsSection from '@/_components/manar/pages-components/home/news-section/NewsSection'
-import ProgrammeSection from '@/_components/manar/pages-components/home/programme/ProgrammeSection'
+// import NewsSection from '@/_components/manar/pages-components/home/news-section/NewsSection'
+// import ProgrammeSection from '@/_components/manar/pages-components/home/programme/ProgrammeSection'
 import QuickLinkBlock from '@/_components/manar/pages-components/home/quick-link-block/QuickLinkBlock'
 import LightCompassSection from '@/_components/manar/pages-components/home/light-compass-section/LightCompassSection'
 import PlanYourVisitSection from '@/_components/manar/pages-components/home/plan-your-visit-section/PlanYourVisitSection'
 import CuratorialTeamSection from '@/_components/manar/pages-components/home/curatorial-team-section/CuratorialTeamSection'
 import ArtistSection from '@/_components/manar/pages-components/home/participating-artists-section/ParticipatingArtistsSection'
+import { Suspense } from 'react'
+import SkeletonLoader from '@/_components/manar/_ui/skeleton-loader/SkeletonLoader'
 
 const ExploreMapSection = dynamic(() => import('@/_components/manar/pages-components/home/explore-map-section/ExploreMapSection'), { ssr: false, loading: () => <div /> })
+const ProgrammeSection = dynamic(() => import('@/_components/manar/pages-components/home/programme/ProgrammeSection'), { ssr: false, loading: () => <div /> })
+const NewsSection = dynamic(() => import('@/_components/manar/pages-components/home/news-section/NewsSection'), { ssr: false, loading: () => <div /> })
 
 export const HomePageRegistry: Record<string, React.FC<{ data: any }>> = {
   banner: ({ data }) => <HeroSection data={data} />,
@@ -19,16 +23,28 @@ export const HomePageRegistry: Record<string, React.FC<{ data: any }>> = {
   'introduction-block': ({ data }) => <LightCompassSection data={data} />,
 
   // TODO: it is static data render
-  'news-carousel': ({ data }) => <NewsSection data={data} />,
+  'news-carousel': ({ data }) => (
+    <Suspense fallback={<SkeletonLoader className="h-150" />}>
+      <NewsSection data={data} />
+    </Suspense>
+  ),
   'plan-your-visit-block': ({ data }) => <PlanYourVisitSection data={data} />,
 
   // TODO: change the name of this programme-carousel to programme-block
   'programme-block': ({ data }) => {
-    return data?.events?.length > 0 ? <ProgrammeSection data={data} /> : null
+    return data?.events?.length > 0 ? (
+      <Suspense fallback={<SkeletonLoader className="h-150" />}>
+        <ProgrammeSection data={data} />
+      </Suspense>
+    ) : null
   },
   'artist-carousel': ({ data }) => <ArtistSection data={data} />,
 
   // TODO: this component is not dynamic, please pass data the dynamic data to this component
-  'map-view': ({ data }) => <ExploreMapSection data={data} />,
+  'map-view': ({ data }) => (
+    <Suspense fallback={<SkeletonLoader className="h-150" />}>
+      <ExploreMapSection data={data} />
+    </Suspense>
+  ),
   'curator-section': ({ data }) => <CuratorialTeamSection data={data} />,
 }
